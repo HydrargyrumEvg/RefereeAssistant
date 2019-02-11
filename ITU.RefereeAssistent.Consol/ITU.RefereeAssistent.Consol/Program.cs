2785e2a6386a381cc0223e1c5fef2bfc6e277eae
HydrargyrumEvg <vozduhada@yandex.ru>
2019-02-11 20:17:02 +0400

    Первая версия программы, ничего не делает

---
+++ ITU.RefereeAssistent.Consol/ITU.RefereeAssistent.Consol/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITU.RefereeAssistent.Consol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Помошник спортивного судьи");
+            Console.ReadKey();
+        }
+    }
+}
